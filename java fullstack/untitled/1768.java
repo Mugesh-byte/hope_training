40432708213c31cf44a25c94f6ac391d04f8d831
Mugesh-byte <mugeshramesh063@gmail.com>
2026-03-26 06:42:19 +0530

    day6

---
+++ java fullstack/untitled/1768.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int n1 = word1.length();
+        int n2 = word2.length();
+        for (int i = 0; i < Math.max(n1, n2); i++) {
+            if (i < n1) {
+                result.append(word1.charAt(i));
+            }
+            if (i < n2) {
+                result.append(word2.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}
