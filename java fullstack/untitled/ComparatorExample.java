130093656db7fc159f7f0313f642e79f1a74f259
Mugesh-byte <mugeshramesh063@gmail.com>
2026-04-10 08:26:58 +0530

    day 16

---
+++ java fullstack/untitled/ComparatorExample.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+class Students{
+    int id;
+    String name;
+    int mark;
+    Students(int id,String name,int mark){
+        this.id=id;
+        this.name=name;
+        this.mark=mark;
+    }
+    @Override
+    public String toString(){
+        return id+"-"+name+"-"+mark;
+    }
+}
+class ComparatorExample{
+    public static void main(String[] args){
+        List<Students> students=new ArrayList<>();
+        students.add(new Students(6,"x",20));
+        students.add(new Students(4,"y",45));
+        students.add(new Students(1,"z",76));
+        students.add(new Students(9,"m",80));
+        students.add(new Students(5,"i",99));
+
+        Comparator<Students> byId=new Comparator<Students>(){
+            public int compare(Students a,Students b){
+                return a.id-b.id;
+            }
+        };
+        Collections.sort(students,byId);
+        System.out.println(students);
+        
+    }
+}
---
+++ java fullstack/untitled/Task.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Product{
+    String name;
+    int price;
+    double rating;
+    Product(String name,int price,double rating){
+        this.name=name;
+        this.price=price;
+        this.rating=rating;
+    }
+    @Override
+    public String toString(){
+        return name+"-"+price+"-"+rating;
+    }
+}
+
+class Task{
+    public static void main(String[] args) {
+        List<Product> products=new ArrayList<>();
+        products.add(new Product("pencil",5,4.9));
+        products.add(new Product("pen",4,4.0));
+        products.add(new Product("calculator",400,4.7));
+        products.add(new Product("compass",10,5.0));
+
+        Comparator<Product> byprice=new Comparator<Product>(){
+            public int compare(Product a,Product b){
+                return Integer.compare(a.price,b.price);
+            }
+        };
+        Collections.sort(products,byprice);
+        System.out.println(products);
+    }
+}
